3cddc185b0666b47f71f322647962b505d7a353e
Sandeep Gumaste <73634237+SandeepGumaste@users.noreply.github.com>
2020-11-01 23:06:32 +0530

    1nov20

--- Largest & Smallest.py
+++ Largest & Smallest.py
@@ -1,15 +1,14 @@
 lsf = 0 #largest so far
 ssf = 0 #smallest so far
-print("\n 20, 81, 112, 254, 4, 250\n")
+print("\n 50, 81, 112, 254, 40, 250\n")
 for num in [50, 81, 112, 254, 40, 250]:
     if num > lsf :
         lsf = num
 
 print("Largest so far :", lsf)
 
-for num in [50, 81, 112, 254, 95, 250]:
+for num in [50, 81, 112, 254, 40, 250]:
     if num < lsf:
         lsf = num
-    print(lsf, num)
 
 print("Smallest so far:", lsf)
