3ed45017b4fa770e3a5bde38a3f6e96d57c25d7b
Sandeep Gumaste <73634237+SandeepGumaste@users.noreply.github.com>
2020-11-02 17:44:45 +0530

    Extract a specific letter from the string entered

---
+++ str length with letter extract.py
@@ -0,0 +1,25 @@
+a = input('Enter a word\n')
+e = a
+b = len(a) - 1
+d=-1
+
+for e in e :
+
+    d= d + 1
+    print(d,"-",e)
+
+print('Enter a value less than or equal to',b)
+print('to exract the letter')
+
+while True :
+    c = input()
+    c = int (c)
+        
+    if c <= b :
+        d = a [c]
+        print(d)
+        break
+
+    elif c > b :
+        print("Enter a value less than",b)
+        continue
