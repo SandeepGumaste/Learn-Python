323e1ee46b1f3e7e4cdb1c782955995ebf054493
Sandeep Gumaste <73634237+SandeepGumaste@users.noreply.github.com>
2020-11-04 11:27:27 +0530

    no update-alternate optimised way is a WIP

--- reverse a string entered.py
+++ reverse a string entered.py
@@ -1,9 +1,8 @@
-x = input('Enter a sentence or a word to get it reversed\n')
+x = input('Enter a sentence or a word to get it reversed: ')
 z = list(x)
 a = z
 y = len(z) - 1
 for x in x :
     a[y] = x
     y = y - 1
-
-print(''.join(a)) 
+print('\n The entered word/sentence in reverse is:',''.join(a))
