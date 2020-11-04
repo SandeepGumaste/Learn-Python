34f8fd92c289d5631c3c3c18b4914f6a87338c76
Sandeep Gumaste <73634237+SandeepGumaste@users.noreply.github.com>
2020-11-04 11:04:13 +0530

    REV ENTERED STRING

--- reverse a string entered.py
+++ reverse a string entered.py
@@ -1,6 +1,9 @@
-x = input('--Enter a word to get it reversed--\n')
-z = x
-y = len(x) - 1
+x = input('Enter a sentence or a word to get it reversed\n')
+z = list(x)
+a = z
+y = len(z) - 1
 for x in x :
-    print(z[y])
-    y=y-1
+    a[y] = x
+    y = y - 1
+
+print(''.join(a)) 
