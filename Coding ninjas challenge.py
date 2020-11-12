85b6d49cdc23b9f698e5a26fc090113cfa1faaaa
Sandeep Gumaste <73634237+SandeepGumaste@users.noreply.github.com>
2020-11-12 13:57:12 +0530

    Challenge completed

---
+++ Coding ninjas challenge.py
@@ -0,0 +1,16 @@
+#input = aaabbcccbd // output = abcbd. checking for consecutive duplicates and remove them.
+x = input('Enter a string : ')
+y = list(x.strip())
+ya = y[1:]
+m = list()
+ya.append(y[0])
+z = 0
+l = len(y)
+while z < l:
+    if y[z] == ya[z]:
+        pass
+    else:
+        m.append(y[z])
+    z = z + 1
+s = ''.join(m)
+print(s)
